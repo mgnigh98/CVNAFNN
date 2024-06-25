8f44896ba505bea0774483fb59fd67f2166f3350
Tim <mgnigh98@gmail.com>
2024-06-25 13:06:08 -0400

    allows switching between different datatypes and torch to complextorch

--- CVNAFNet.py
+++ CVNAFNet.py
@@ -1,26 +1,24 @@
 import torch as T
 import complextorch as cT
-from complextorch.nn.modules.conv import CVConv2d, CVConvTranpose2d
-from complextorch.nn.modules.attention.eca import CVEfficientChannelAttention2d
-from complextorch.nn.modules.layernorm import CVLayerNorm
-from complextorch.nn.modules.batchnorm import CVBatchNorm2d
-from complextorch.nn.modules.dropout import CVDropout
 from complextorch.nn import functional as cF
 
 from torch.nn import functional as F
 
-__all__ = ["CVNAFNet", "CVNAFEnc", "CVDownSample"]
+__all__ = ["CVNAFNet", "CVNAFEnc", "DownSample"]
 
-class DynamicCVLayerNorm(T.nn.Module):
+t = cT
+
+
+class DynamicLayerNorm(T.nn.Module):
     def __init__(self):
-        super(DynamicCVLayerNorm, self).__init__()
+        super(DynamicLayerNorm, self).__init__()
 
     def forward(self, x):
         # Calculate the shape dynamically based on the input tensor size
         normalized_shape = x.size(-1)
         
         # Apply LayerNorm with dynamically calculated shape
-        return CVLayerNorm(normalized_shape)(x)
+        return t.nn.LayerNorm(normalized_shape)(x)
 
 
 class SimpleGate(T.nn.Module):
@@ -30,28 +28,28 @@ class SimpleGate(T.nn.Module):
 
 
 class CVNAFBlock(T.nn.Module):
-    def __init__(self, c, DW_Expand=2, FFN_Expand=2, drop_out_rate=0.):
+    def __init__(self, c, DW_Expand=2, FFN_Expand=2, drop_out_rate=0., dtype=T.complex64):
         super().__init__()
         dw_channel = c * DW_Expand
-        self.conv1 = CVConv2d(in_channels=c, out_channels=dw_channel, kernel_size=1, padding=0, stride=1, groups=1, bias=True)
-        self.conv2 = CVConv2d(in_channels=dw_channel, out_channels=dw_channel, kernel_size=3, padding=1, stride=1, groups=dw_channel,
-                               bias=True)
-        self.conv3 = CVConv2d(in_channels=dw_channel // 2, out_channels=c, kernel_size=1, padding=0, stride=1, groups=1, bias=True)
+        self.conv1 = t.nn.Conv2d(in_channels=c, out_channels=dw_channel, kernel_size=1, padding=0, stride=1, groups=1, bias=True, dtype=dtype)
+        self.conv2 = t.nn.Conv2d(in_channels=dw_channel, out_channels=dw_channel, kernel_size=3, padding=1, stride=1, groups=dw_channel,
+                               bias=True, dtype=dtype)
+        self.conv3 = t.nn.Conv2d(in_channels=dw_channel // 2, out_channels=c, kernel_size=1, padding=0, stride=1, groups=1, bias=True, dtype=dtype)
         
-        self.sca = CVEfficientChannelAttention2d(dw_channel)
+        self.sca = cT.nn.EfficientChannelAttention2d(dw_channel)
 
         # SimpleGate
         self.sg = SimpleGate()
 
         ffn_channel = FFN_Expand * c
-        self.conv4 = CVConv2d(in_channels=c, out_channels=ffn_channel, kernel_size=1, padding=0, stride=1, groups=1, bias=True)
-        self.conv5 = CVConv2d(in_channels=ffn_channel // 2, out_channels=c, kernel_size=1, padding=0, stride=1, groups=1, bias=True)
+        self.conv4 = t.nn.Conv2d(in_channels=c, out_channels=ffn_channel, kernel_size=1, padding=0, stride=1, groups=1, bias=True, dtype=dtype)
+        self.conv5 = t.nn.Conv2d(in_channels=ffn_channel // 2, out_channels=c, kernel_size=1, padding=0, stride=1, groups=1, bias=True, dtype=dtype)
 
-        self.norm1 = DynamicCVLayerNorm()
-        self.norm2 = DynamicCVLayerNorm()
+        self.norm1 = DynamicLayerNorm()
+        self.norm2 = DynamicLayerNorm()
 
-        self.dropout1 = CVDropout(drop_out_rate) if drop_out_rate > 0. else T.nn.Identity()
-        self.dropout2 = CVDropout(drop_out_rate) if drop_out_rate > 0. else T.nn.Identity()
+        self.dropout1 = t.nn.Dropout(drop_out_rate) if drop_out_rate > 0. else T.nn.Identity()
+        self.dropout2 = t.nn.Dropout(drop_out_rate) if drop_out_rate > 0. else T.nn.Identity()
 
         self.beta = T.nn.Parameter(T.zeros((1, c, 1, 1)), requires_grad=True)
         self.gamma = T.nn.Parameter(T.zeros((1, c, 1, 1)), requires_grad=True)
@@ -79,16 +77,16 @@ class CVNAFBlock(T.nn.Module):
 
 class CVNAFNet(T.nn.Module):
 
-    def __init__(self, img_channel=1, width=16, middle_blk_num=1, enc_blk_nums=[1,1], dec_blk_nums=[1,1], scale=1, ):
+    def __init__(self, img_channel=1, width=16, middle_blk_num=1, enc_blk_nums=[1,1], dec_blk_nums=[1,1], scale=1, dtype=T.complex64):
         super().__init__()
         self.scale = scale
 
-        self.intro = CVConv2d(in_channels=img_channel, out_channels=width, kernel_size=3, padding=1, stride=1, groups=1,
-                              bias=True)
-        self.ending = CVConv2d(in_channels=width, out_channels=img_channel, kernel_size=3, padding=1, stride=1, groups=1,
-                              bias=True)
-        self.upscale = CVConvTranpose2d(in_channels=width, out_channels=img_channel, kernel_size=scale+2, padding=1, stride=scale, groups=1,
-                              bias=True)
+        self.intro = t.nn.Conv2d(in_channels=img_channel, out_channels=width, kernel_size=3, padding=1, stride=1, groups=1,
+                              bias=True, dtype=dtype)
+        self.ending = t.nn.Conv2d(in_channels=width, out_channels=img_channel, kernel_size=3, padding=1, stride=1, groups=1,
+                              bias=True, dtype=dtype)
+        self.upscale = t.nn.ConvTranpose2d(in_channels=width, out_channels=img_channel, kernel_size=scale+2, padding=1, stride=scale, groups=1,
+                              bias=True, dtype=dtype)
                               #explore for in_channels on ending: int(width*(2**len(enc_blk_nums))/(2**len(dec_blk_nums)))
 
         self.encoders = T.nn.ModuleList()
@@ -102,30 +100,30 @@ class CVNAFNet(T.nn.Module):
             #print(f"encoders {num}")
             self.encoders.append(
                 T.nn.Sequential(
-                    *[CVNAFBlock(chan) for _ in range(num)]
+                    *[CVNAFBlock(chan, dtype=dtype) for _ in range(num)]
                 )
             )
             self.downs.append(
-                CVConv2d(chan, 2*chan, 2, 2)
+                t.nn.Conv2d(chan, 2*chan, 2, 2, dtype=dtype)
             )
             chan = chan * 2
 
         self.middle_blks = \
             T.nn.Sequential(
-                *[CVNAFBlock(chan) for _ in range(middle_blk_num)]
+                *[CVNAFBlock(chan, dtype=dtype) for _ in range(middle_blk_num)]
             )
 
         for num in dec_blk_nums:
             self.ups.append(
                 T.nn.Sequential(
-                    CVConv2d(chan, chan * 2, 1, bias=False),
+                    t.nn.Conv2d(chan, chan * 2, 1, bias=False, dtype=dtype),
                     T.nn.PixelShuffle(2)
                 )
             )
             chan = chan // 2
             self.decoders.append(
                 T.nn.Sequential(
-                    *[CVNAFBlock(chan) for _ in range(num)]
+                    *[CVNAFBlock(chan, dtype=dtype) for _ in range(num)]
                 )
             )
 
@@ -184,11 +182,11 @@ class CVNAFNet(T.nn.Module):
         
 class CVNAFEnc(T.nn.Module):
 
-    def __init__(self, img_channel=1, width=16, enc_blk_nums=[1,1], output_hw=16):
+    def __init__(self, img_channel=1, width=16, enc_blk_nums=[1,1], output_hw=16, dtype=T.complex64):
         super().__init__()
 
-        self.intro = CVConv2d(in_channels=img_channel, out_channels=width, kernel_size=3, padding=1, stride=1, groups=1,
-                              bias=True)
+        self.intro = t.nn.Conv2d(in_channels=img_channel, out_channels=width, kernel_size=3, padding=1, stride=1, groups=1,
+                              bias=True, dtype=dtype)
         
 
         self.encoders = T.nn.ModuleList()
@@ -199,19 +197,19 @@ class CVNAFEnc(T.nn.Module):
             #print(f"encoders {num}")
             self.encoders.append(
                 T.nn.Sequential(
-                    *[CVNAFBlock(chan) for _ in range(num)]
+                    *[CVNAFBlock(chan, dtype=dtype) for _ in range(num)]
                 )
             )
             self.downs.append(
-                CVConv2d(chan, 2*chan, 4, 2)
+                t.nn.Conv2d(chan, 2*chan, 4, 2)
             )
             chan = chan * 2
-        self.ending = CVConv2d(in_channels=chan, out_channels=img_channel, kernel_size=3, padding=1, stride=1, groups=1,
-                              bias=True)
+        self.ending = t.nn.Conv2d(in_channels=chan, out_channels=img_channel, kernel_size=3, padding=1, stride=1, groups=1,
+                              bias=True, dtype=dtype)
 
 
         self.padder_size = 2 ** len(self.encoders)
-        self.pool = cT.nn.CVAdaptiveAvgPool2d(output_size=output_hw)
+        self.pool = t.nn.AdaptiveAvgPool2d(output_size=output_hw)
 
     def forward(self, inp):
         B, C, H, W = inp.shape
@@ -243,11 +241,11 @@ class CVNAFEnc(T.nn.Module):
         x = F.pad(x, (0, mod_pad_w, 0, mod_pad_h))
         return x
 
-class CVDownSample(T.nn.Module):
+class DownSample(T.nn.Module):
     def __init__(self, input_size, downsample_rate=1):
         super().__init__()
         self.down = downsample_rate
-        self.pool = cT.nn.CVAdaptiveAvgPool2d(input_size//downsample_rate)
+        self.pool = t.nn.AdaptiveAvgPool2d(input_size//downsample_rate)
 
     def forward(self, x):
         return self.pool(x)
